4c2ba637de9224a8a41c2064c94122cd5ca51f22
Crys Carmona <crysc4rmon4@gmail.com>
2025-09-28 13:35:01 +0000

    proyecto final de excusas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+
+let who = ['Mi abuela invalida', 'El cartero ciego', 'Mi pez zombie', 'El gato psicópata'];
+let action = ['devoró', 'escondió', 'aplastó', 'quemó'];
+let what = ['mi tarea', 'mi teléfono', 'el carro', 'mi computadora'];
+let when = ['antes de la clase', 'mientras dormía', 'durante mi almuerzo', 'mientras rezaba por mi vida'];
+
+
+function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    document.getElementById("excuse").innerHTML = excuse;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  generateExcuse(); 
+
+   const button = document.getElementById("new-excuse-btn");
+    if(button) { 
+        button.onclick = generateExcuse;
+         }
 };
+
+
